5592b14aad3d70b9ae92b6b64b0dc67e24c8e1ce
shooozz <vodolb@mail.ru>
2023-04-15 15:43:04 +0300

    add all files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className='title'>
+          <h1>Moscow</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
